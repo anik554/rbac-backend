c7d49c74ab638833736a9ab7e242db79da292dd6
anik924 <anikimtiaz924@gmail.com>
2026-03-15 02:23:36 +0600

    netlify config file updated

---
+++ netlify/functions/api.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+var main_1 = require("../../src/main");
+Object.defineProperty(exports, "handler", { enumerable: true, get: function () { return main_1.handler; } });
+//# sourceMappingURL=api.js.map
