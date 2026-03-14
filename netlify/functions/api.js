d9fab501ca05b08dab03502b81e36d2c4ee0bbe8
anik924 <anikimtiaz924@gmail.com>
2026-03-15 02:38:54 +0600

    bug fix

--- netlify/functions/api.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-var main_1 = require("../../src/main");
-Object.defineProperty(exports, "handler", { enumerable: true, get: function () { return main_1.handler; } });
-//# sourceMappingURL=api.js.map
--- netlify/functions/api.ts
+++
@@ -1,3 +0,0 @@
-import { handler } from '../../dist/main';
-
-export { handler };
